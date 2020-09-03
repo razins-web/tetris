9dec33974620b3eab507702cfbf7376fcfedb094
razins-web <aleksandrs.razins98@gmail.com>
2020-09-03 15:19:12 +0300

    add commit

--- figure_manager.js
+++ figure_manager.js
@@ -1,199 +1,371 @@
-let figureManager = {
+let buttons = document.querySelectorAll('button');
 
-  createFigureOne: function(x, y, r) {
-    let color = "yellow";
-    switch (r) {
+document.addEventListener("keydown", function(){
+  switch (event.code) {
+    case "ArrowLeft":
+      console.log("left key pressed");
+      break;
+    case "ArrowRight":
+      console.log("right key pressed");
+      break;
+    case "ArrowDown":
+      console.log("down key pressed");
+      break;
+    case "Space":
+      console.log("Space key pressed");
+      break;
+  }
+});
+
+/*for (let i=0; i < 200; i++) {
+  game_field.appendChild(
+    emptyDiv.cloneNode()
+  );
+
+  document.getElementsById('example-field').appendChild(
+    document.createElemenet('div')
+  );
+}*/
+//if функция делает цикл в своей же функции
+
+let figureManager = {
+ game_field: null,
+  x: 0,
+  y: 0,
+  state: true,
+   figureFall: function(y){ // figurefall рекрусивная функция запускает функцию по новому.
+     let obj = this; // this Это тот же самый figureManager
+     setTimeout(function(){
+       //alert("Hello");
+         if(obj.state){
+           //obj.createFigureOne(2, y++, 1)
+           //obj.createFigureTwo(5, y++, 1)
+           //obj.createFigureThree(3, y++, 1)
+           //obj.createFigureFour(5, y++, 2)
+           obj.createFigureFive(5, y++, 1)
+           //obj.createFigureSix(3, y++, 1)
+           //obj.createFigureSeven(3, y++, 1)
+           obj.figureFall(y); // Вызывает функцию figureFall: function(y) которая вызывает figureManager.figureFall(0);
+       }
+     }, 300);
+   },
+
+  createFigureOne: function (x, y, r) {
+    let color = "";
+      switch (r) {
       case 1:
-      case 3:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x+1, y, color);
-        this.changeCubeColor(x, y+1, color);
-        this.changeCubeColor(x+1, y+1, color);
-        break;
-      case 2:
-      case 4:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x+1, y, color);
-        this.changeCubeColor(x, y+1, color);
-        this.changeCubeColor(x+1, y+1, color);
+        console.log(" y = " + y + ", this.y = " + this.y);
+        for (let n = 1; n <= 2; n++) {
+          if (n == 2) // ФОрмула с for для выполнения кол-ва цикла  ниже.
+          {
+            color = "yellow";
+          }
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x, this.y-1, color);
+          this.changeCubeColor(++this.x, this.y--, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.y = y;
+          this.x = x;
+        }
     }
   },
 
   createFigureTwo: function(x, y, r) {
-    let color = "skyblue";
-    switch (r) {
-      case 1:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x--, --y, color);
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x-1, y, color);
+      let color = "";
+      for (let n = 1; n <= 2; n++) {
+        if (n == 2) {
+          color = "skyblue";
+        }
+        let coords = [
+          [this.x, this.y],
+          [++this.x, this.y],
+          [this.x+1, this.y],
+          [this.x+2, this.y]
+        ];
+        switch (r) {
+        case 1:
+        if (n == 2) {
+          this.validate(coords);
+          }
+        for (coord of coords) {
+          this.changeCubeColor(coord[0], coord[1], color);
+        }
         break;
-      case 3:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x++, ++y, color);
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x+1, y, color);
-        break;
-      case 2:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(++x, y--, color);
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x, y-1, color);
-        break;
-      case 4:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(--x, y++, color);
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x, y+1, color);
+        case 3:
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x++, ++this.y, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x+1, this.y, color);
+          break;
+        case 2:
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(++this.x, this.y--, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x, this.y-1, color);
+          break;
+        case 4:
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(--this.x, this.y++, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x, this.y+1, color);
+      }
+      this.y = y;
+      this.x = x;
      }
    },
 
   createFigureThree: function(x, y, r) {
-      let color = "red"; // To do rotation
-      switch (r) {
-      case 1:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(++x, y, color);
-        this.changeCubeColor(x+1, y, color);
-        this.changeCubeColor(x+2, y, color);
-        break;
-      case 3:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(--x, y, color);
-        this.changeCubeColor(x-1, y, color);
-        this.changeCubeColor(x-2, y, color);
-        break;
-      case 2:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x, ++y, color);
-        this.changeCubeColor(x, y+1, color);
-        this.changeCubeColor(x, y+2, color);
-        break;
-      case 4:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x, --y, color);
-        this.changeCubeColor(x, y-1, color);
-        this.changeCubeColor(x, y-2, color);
+    let color = ""; // To do rotation
+    for (let n = 1; n <= 2; n++) {
+      if (n == 2) {
+        color = "red";
       }
-    },
-  createFigureFour: function(x, y, r) {
-      let color = "orange";
-      switch (r) {
-      case 1:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(++x, --y, color);
-        this.changeCubeColor(x+1, y++, color);
-        this.changeCubeColor(x, y, color);
-        break;
-      case 3:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x++, ++y, color);
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x, y+1, color);
-        break;
-      case 2:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(--x, y++, color);
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x-1, y, color);
-        break;
-      case 4:
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x--, --y, color);
-        this.changeCubeColor(x, y, color);
-        this.changeCubeColor(x, y-1, color);
+      let coords = [
+        [this.x, this.y],
+        [++this.x, this.y],
+        [this.x+1, this.y],
+        [this.x+2, this.y]
+      ];
+    switch (r) {
+    case 1:
+    if (n == 2) {
+      this.validate(coords);
       }
-    },
-  createFigureFive: function(x, y, r) {
-        let color = "green";
-        switch (r) {
+    for (coord of coords) {
+      this.changeCubeColor(coord[0], coord[1], color);
+    }
+      break;
+    case 3:
+      this.changeCubeColor(this.x, this.y, color);
+      this.changeCubeColor(--this.x, this.y, color);
+      this.changeCubeColor(this.x-1, this.y, color);
+      this.changeCubeColor(this.x-2, this.y, color);
+      break;
+    case 2:
+      this.changeCubeColor(this.x, this.y, color);
+      this.changeCubeColor(this.x, ++this.y, color);
+      this.changeCubeColor(this.x, this.y+1, color);
+      this.changeCubeColor(this.x, this.y+2, color);
+      break;
+    case 4:
+      this.changeCubeColor(this.x, this.y, color);
+      this.changeCubeColor(this.x, --this.y, color);
+      this.changeCubeColor(this.x, this.y-1, color);
+      this.changeCubeColor(this.x, this.y-2, color);
+    }
+    this.y = y;
+    this.x = x;
+    }
+  },
+createFigureFour: function(x, y, r) {
+    let color = "";
+    for (let n = 1; n <= 2; n++) {
+      if (n == 2) {
+        color = "orange";
+      }
+      let coords = [];
+      coords = [
+        [this.x, this.y],
+        [++this.x, this.y],
+        [this.x+1, this.y],
+        [this.x+2, this.y]
+      ];
+      switch (r) {
         case 1:
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(x, --y, color);
-          this.changeCubeColor(x-1, y, color);
-          this.changeCubeColor(x+1, y, color);
-          break;
         case 3:
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(++x, y, color);
-          this.changeCubeColor(x, y-1, color);
-          this.changeCubeColor(x, y+1, color);
-          break;
+        coords = [
+          [this.x, this.y],
+          [this.x, --this.y],
+          [++this.x, this.y],
+          [this.x, --this.y]
+        ];
+        break;
         case 2:
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(x, ++y, color);
-          this.changeCubeColor(x+1, y, color);
-          this.changeCubeColor(x-1, y, color);
-          break;
         case 4:
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(--x, y, color);
-          this.changeCubeColor(x, y+1, color);
-          this.changeCubeColor(x, y-1, color);
+        coords = [
+          [this.x, this.y],
+          [--this.x, this.y],
+          [this.x, --this.y],
+          [--this.x, this.y]
+        ];
+        break;
+    }
+    if (n == 2) {
+      this.validate(coords);
+      }
+    for (coord of coords) {
+      this.changeCubeColor(coord[0], coord[1], color);
+    }
+    this.y = y;
+    this.x = x;
+    }
+},
+  createFigureFive: function(x, y, r) {
+        let color = "";
+        for (let n = 1; n <= 2; n++) {
+          if (n == 2) {
+            color = "green";
+          }
+          let coords = [];
+          switch (r) {
+            case 1:
+            coords = [
+              [this.x, this.y],
+              [++this.x, this.y],
+              [this.x, this.y-1],
+              [this.x, this.y+1]
+            ];
+            break;
+            case 3:
+            coords = [
+              [this.x, this.y],
+              [this.x, --this.y],
+              [this.x-1, this.y],
+              [this.x+1, this.y]
+            ];
+            break;
+            case 2:
+            coords = [
+              [this.x, this.y],
+              [this.x, ++this.y],
+              [this.x-1, this.y],
+              [this.x+1, this.y]
+            ];
+            break;
+            case 4:
+            coords = [
+              [this.x, this.y],
+              [--this.x, this.y],
+              [this.x, this.y+1],
+              [this.x, this.y-1]
+            ];
+            break;
+        }
+        if (n == 2) {
+          this.validate(coords);
+          }
+        for (coord of coords) {
+          this.changeCubeColor(coord[0], coord[1], color);
+      }
+      this.y = y;
+      this.x = x;
       }
     },
   createFigureSix: function(x, y, r) {
-        let color = "Purple";
+        let color = "";
+        for (let n = 1; n <= 2; n++) {
+          if (n == 2) {
+            color = "purple";
+          }
         switch (r) {
         case 1:
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(++x, y++, color);
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(x+1, y, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(++this.x, this.y++, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x+1, this.y, color);
           break;
         case 3:
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(x, ++y, color);
-          this.changeCubeColor(--x, y, color);
-          this.changeCubeColor(x, y+1, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x, ++this.y, color);
+          this.changeCubeColor(--this.x, this.y, color);
+          this.changeCubeColor(this.x, this.y+1, color);
           break;
         case 2:
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(--x, y, color);
-          this.changeCubeColor(x, --y, color);
-          this.changeCubeColor(x-1, y, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(--this.x, this.y, color);
+          this.changeCubeColor(this.x, --this.y, color);
+          this.changeCubeColor(this.x-1, this.y, color);
           break;
         case 4:
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(x++, --y, color);
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(x, y-1, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x++, --this.y, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x, this.y-1, color);
+        }
+        this.y = y;
+        this.x = x;
         }
     },
 
   createFigureSeven: function(x, y, r) {
-        let color = "grey";
+        let color = "";
+        for (let n = 1; n <= 2; n++) {
+          if (n == 2) {
+            color = "grey";
+          }
         switch (r) {
         case 1:
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(x++, ++y, color);
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(x+1, y, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x++, ++this.y, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x+1, this.y, color);
           break;
         case 3:
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(x, --y, color);
-          this.changeCubeColor(--x, y, color);
-          this.changeCubeColor(x-1, y, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x, --this.y, color);
+          this.changeCubeColor(--this.x, this.y, color);
+          this.changeCubeColor(this.x-1, this.y, color);
           break;
         case 2:
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(--x, y++, color);
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(x, y+1, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(--this.x, this.y++, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x, this.y+1, color);
           break;
         case 4:
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(++x, y--, color);
-          this.changeCubeColor(x, y, color);
-          this.changeCubeColor(x, y-1, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(++this.x, this.y--, color);
+          this.changeCubeColor(this.x, this.y, color);
+          this.changeCubeColor(this.x, this.y-1, color);
+      }
+      this.y = y;
+      this.x = x;
       }
     },
 
-  changeCubeColor: function(x, y, color) {
+
+  changeCubeColor: function(x, y, color, r) {
+        if (y == 20) {
+          this.state = false;
+        }
+
         let whole_line = y-1;
         let nth = whole_line*10 + x;
-        let cube = game_field.querySelector('div:nth-child(' + nth + ')');
+        let cube = this.game_field.querySelector('div:nth-child(' + nth + ')');
+
+        if (y < 1 || y > 20 || x < 1 || x > 10 ) {
+          return;
+        }
+
+        if (color == '') {
+          cube.classList.remove('colored');
+        }
+        else {
+        cube.classList.add('colored');
+        }
         cube.style.background = color;
+    },
+    resetObject: function () {
+      this.x = 0;
+      this.y = 0;
+      this.state = true;
+    },
+    validate: function(coords) {
+      console.log(coords)
+      for (coord of coords) {
+        let x = coord[0],
+            y = coord[1]+1;
+        if (y < 1 || y > 20 || x < 1 || x > 10 ) {
+          return;
+        }
+        let whole_line = y-1;
+        let nth = whole_line*10 + x;
+        let cube = this.game_field.querySelector('div:nth-child(' + nth + ')');
+
+        if (cube.classList.contains('colored')) {
+          this.state = false;
+          return;
+        }
+      }
     }
   }
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let game_field = document.querySelector('#game-field');
-
+// let emptyDiv = document.createElement('div');
 /* let z = 10;
 
 console.log(++z); Меняет всю переменную и в дальнейшем в функции перед числом Z
@@ -27,12 +27,12 @@ let n = [[3,3,3] 3, 4] - Масив
 console.log(cars.getColor("white"));
 */
 
-let cars = function() {
+/*let cars = function() {
   this.specification = {
     size: 32,
     number: 32512
   }; //отделяем запятой след объект, а последний не ставим ничего
-}; // Прототип
+}; // Констрактор
 
 let audi = new cars();
 audi.specification.size = 50;
@@ -46,12 +46,28 @@ console.log(bmw.specification);
 
 audi = new cars();
 
-console.log(audi.specification);
+console.log(audi.specification); */
 
-figureManager.createFigureOne(1, 1, 1)
-figureManager.createFigureTwo(7, 14, 3)
+figureManager.game_field = game_field;
+
+figureManager.createFigureThree(7, 14, 3)
+figureManager.resetObject();
+figureManager.figureFall(0);
+
+/*
 figureManager.createFigureThree(7, 11, 4)
 figureManager.createFigureFour(7, 3, 4)
 figureManager.createFigureFive(3, 8, 4)
 figureManager.createFigureSix(3, 4, 4)
-figureManager.createFigureSeven(3, 16, 4)
+figureManager.createFigureSeven(3, 16, 4)*/
+
+/*let second_game_field = document.querySelector('#second-game-field');
+figureManager.game_field = second_game_field;
+console.log(second_game_field);
+figureManager.createFigureOne(5, 2, 1)
+figureManager.createFigureTwo(7, 14, 1)
+figureManager.createFigureThree(5, 13, 1)
+figureManager.createFigureFour(5, 5, 1)
+figureManager.createFigureFive(6, 11, 1)
+figureManager.createFigureSix(5, 7, 1)
+figureManager.createFigureSeven(5, 17, 1)*/
